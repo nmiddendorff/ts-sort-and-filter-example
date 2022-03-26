307d2050ce9e2c2c3d630687b8620e347ccfe76d
nmiddendorff <middendorff.nelson@gmail.com>
2022-03-26 16:40:00 -0500

    first

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,85 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { FC, useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
 
+const Spinner = () => 
+  <span className={styles.spinner}></span>
+
+
+  // https://jsonplaceholder.typicode.com/users
+
+interface person {
+  name: string
+  id: string
+  email: string
+}
+
 const Home: NextPage = () => {
+  const [data, setData] = useState<person[]>([]);
+  const [filteredData, setFilteredData] = useState<person[]>([]);
+  const [loadingState, setLoadingState] = useState(false);
+  const [searchInput, setsearchInput] = useState('');
+  const [resultsDirection, setResultsDirection] = useState('Default');
+
+  useEffect(() => {
+    setLoadingState(true)
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data)
+        setFilteredData(data)
+        setLoadingState(false)
+      })
+  }, [])
+
+
+  const sortAndFilter = (enteredName: string, sortOption: string) => {
+    const lowerCaseInput = enteredName.toLowerCase();
+
+    let dataSorted = [...data];
+
+    if (sortOption === 'Default'){
+      console.log(data)
+      dataSorted = data;
+    } else {
+      dataSorted = dataSorted.sort(function(a, b){
+        if(sortOption === 'AZ'){
+          if(a.name < b.name) { return -1; }
+          if(a.name > b.name) { return 1; }
+        } else if(sortOption === 'ZA'){
+          if(a.name < b.name) { return 1; }
+          if(a.name > b.name) { return -1; }
+        }
+        return 0;
+      })
+    }
+
+    const foundItems = dataSorted.filter(function(user) {
+
+      return user.name.toLowerCase().indexOf(lowerCaseInput) > -1; 
+    });
+
+    setFilteredData(foundItems);
+  }
+
+  const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const enteredName = event.target.value;
+
+    setsearchInput(enteredName);
+    sortAndFilter(enteredName, resultsDirection);
+  };
+
+  const selectHandler = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const sortOption = event.target.value;
+
+    setResultsDirection(sortOption);
+    sortAndFilter(searchInput, sortOption);
+  };
+
+  const sortOptions: string[] = ['Default', 'AZ', 'ZA'];
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,58 +89,28 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      {
+        loadingState ? <Spinner /> : null
+      }
+      <div className={styles.peopleContainer}>
+        <input className={styles.searchInput} value={searchInput} onChange={inputHandler} />
+        <select onChange={selectHandler} name="cars" defaultValue={resultsDirection} >
+          {sortOptions.map((option) => { 
+              return (<option key={option} value={option}>{option}</option>);
+          })}
+        </select>
+        { 
+          filteredData.map((item) => {
+            return <div className={styles.personCard} key={item.id}>
+              <div className={styles.personName}>{item.name}</div>
+              <div className={styles.email}>{item.email}</div>
+            </div>
+          })
+        }  
+      </div>
+      <pre>{JSON.stringify(data,  null, 2)}</pre>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
